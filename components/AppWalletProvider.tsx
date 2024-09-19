075d3ece838cb4ba36ca94af63b6e2f281893f02
Vishnu Kumar <vishnukumart96@gmail.com>
2024-09-19 21:35:33 +0530

    feat: initialise repo

---
+++ components/AppWalletProvider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { FC, ReactNode } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import {
+    WalletModalProvider,
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+// Default styles that can be overridden by your app
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+export const AppWalletProvider: FC<{children:ReactNode}> = ({children}) => {
+    return (
+        <ConnectionProvider endpoint={clusterApiUrl("devnet")}>
+            <WalletProvider wallets={[]} autoConnect>
+                <WalletModalProvider>
+                    {children}
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
---
+++ components/Navbar.tsx
@@ -0,0 +1,32 @@
+"use client";
+import dynamic from 'next/dynamic';
+import { useWallet } from "@solana/wallet-adapter-react";
+
+const WalletMultiButton = dynamic(
+    async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+    { ssr: false }
+);
+
+const WalletDisconnectButton = dynamic(
+    async () => (await import('@solana/wallet-adapter-react-ui')).WalletDisconnectButton,
+    { ssr: false }
+);
+
+export const Navbar = () => {
+
+    const {wallet} = useWallet();
+
+    return (
+        <div className="flex items-center justify-between px-8 py-3 w-screen border">
+
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                Tuber
+            </h1>
+
+            {
+                wallet ? <WalletDisconnectButton /> : <WalletMultiButton/>
+            }
+
+        </div>
+    )
+}
